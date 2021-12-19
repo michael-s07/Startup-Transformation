2b5f9e77073f05f1132768c2dbaa2272f11d22bf
michael-s07 <michael.sosu@amalitech.org>
2021-12-19 20:29:08 +0000

    updates

--- script.py
+++ script.py
@@ -9,3 +9,59 @@ import numpy as np
 financial_data = pd.read_csv('financial_data.csv')
 
 # code goes here
+print(financial_data.head())
+month= financial_data.Month
+revenue= financial_data.Revenue
+expenses= financial_data.Expenses
+
+plt.plot(month, revenue)
+plt.xlabel('Month')
+plt.ylabel('Amount ($)')
+plt.title('Revenue')
+plt.show()
+
+plt.clf
+plt.plot(month, expenses)
+plt.xlabel('Month')
+plt.ylabel('Expenses')
+plt.title('Expenses')
+plt.show()
+
+# Loading the expenses data set
+expense_overview = pd.read_csv('expenses.csv')
+print(expense_overview.head(7))
+
+expense_categories = expense_overview.Expense
+proportions = expense_overview.Proportion
+
+#Pie Plot 
+expense_categories = ['Salaries', 'Advertising', 'Office Rent', 'Other']
+proportions = [0.62, 0.15, 0.15, 0.08]
+plt.clf()
+plt.pie(proportions, labels = expense_categories)
+plt.title('Expense Categories')
+plt.axis('Equal')
+plt.tight_layout()
+plt.show()
+
+expense_cut = 'Salaries'
+
+# Load employees data
+employees = pd.read_csv('employees.csv')
+print(employees.head())
+
+sorted_productivity = employees.sort_values(by=['Productivity'])
+print(sorted_productivity)
+
+employees_cut = sorted_productivity.head(100)
+print(employees_cut)
+
+transformation = 'standardization'
+
+commute_times = employees['Commute Time']
+commute_times_log = np.log(commute_times)
+print(commute_times.describe())
+
+plt.clf()
+plt.hist(commute_times_log)
+plt.show()
